b04d84e3b63cfb22099a744aa358a72fa075c2b1
Yves Goldberg <admin@ygol.com>
2017-01-18 22:31:14 +0200

    ADD conf files for git and tmux and zsh

---
+++ conf/zsh/config/git.zsh
@@ -0,0 +1,32 @@
+compdef g=git
+function g {
+  if [[ $# -gt 0 ]]; then
+    git "$@"
+  else
+    git status --short --branch
+  fi
+}
+
+# function current_branch() {
+#   git rev-parse --git-dir > /dev/null 2>&1 && \
+#     git symbolic-ref --short HEAD
+# }
+
+function gitup {
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+        cd "./$(git rev-parse --show-cdup)"
+    else
+        return
+    fi
+}
+
+function list-all-git-refs() {
+  find .git/refs  -type f -print -exec cat {} \;
+}
+
+_hub() { reply=(browse compare pull-request ci-status) }
+compctl -K _hub hub
+
+edit-modified-files-in-tabs() {
+  vim -O $(git status --porcelain | sed s/^...//)
+}
